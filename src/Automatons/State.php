a0e571a36f48bd371204f00d32e42a10d5a55624
Petr Kessler <kesspess@gmail.com>
2013-05-18 00:56:42 +0200

    microoptimization

--- src/Automatons/State.php
+++ src/Automatons/State.php
@@ -23,7 +23,7 @@ class State
 	/** @param  string $name */
 	function __construct($name)
 	{
-		if (!preg_match('#^\S+$#', $name)) {
+		if (preg_match('#\s#', $name)) {
 			throw new E\InvalidStateNameException('State name must not containt white characters.');
 		}
 
